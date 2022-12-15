de61269a36bac887ccd0fc910524dc31b28320df
Lukas Nickel <lukas.nickel@stud.uni-hannover.de>
2022-12-15 21:46:00 +0100

    solution part 2

--- day02/solution.c
+++ day02/solution.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-#define ROCK 0 //rock
-#define PAPER 1 //paper
-#define SCISSOR 2 //scissor
+#define ROCK 0
+#define PAPER 1
+#define SCISSOR 2
+
+#define LOSE 0
+#define DRAW 1
+#define WIN 2
 
 void solve_problem_1()
 {
@@ -43,8 +47,48 @@ void solve_problem_1()
     printf("Solution 1: %d\n", points);
 }
 
+void solve_problem_2()
+{
+    const char* filepath = "input.txt";
+    FILE* f = NULL;
+    char buffer[8];
+    int points = 0;
+
+    f = fopen(filepath, "r");
+    if (f == NULL) return;
+
+    while (fgets(buffer, sizeof(buffer), f))
+    {
+        int opponent = buffer[0] - 'A';
+        int you = buffer[2] - 'X';
+
+        switch (opponent) {
+            case ROCK : {
+                if (you == LOSE) points += 0 + 3;
+                else if (you == DRAW) points += 3 + 1;
+                else if (you == WIN) points += 6 + 2;
+                break;
+            }
+            case PAPER : {
+                if (you == LOSE) points += 0 + 1;
+                else if (you == DRAW) points += 3 + 2;
+                else if (you == WIN) points += 6 + 3;
+                break;
+            }
+            case SCISSOR : {
+                if (you == LOSE) points += 0 + 2;
+                else if (you == DRAW) points += 3 + 3;
+                else if (you == WIN) points += 6 + 1;
+                break;
+            }
+        }
+    }
+    printf("Solution 2: %d\n", points);
+}
+
 int main()
 {
     solve_problem_1();
+    solve_problem_2();
     return 0;
 }
